65eb1583af65e0bd8390ac80e0b0dc760b5a2de3
hborah <hemangakrishnaborah@gmail.com>
2016-09-11 15:27:57 -0700

    Cleaned up settings in tool bar

--- app/src/main/java/productions/funky/com/smokenomore/Counter.java
+++ app/src/main/java/productions/funky/com/smokenomore/Counter.java
@@ -68,9 +68,9 @@ public class Counter extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        /*if (id == R.id.action_settings) {
             return true;
-        }
+        }*/
 
         return super.onOptionsItemSelected(item);
     }
