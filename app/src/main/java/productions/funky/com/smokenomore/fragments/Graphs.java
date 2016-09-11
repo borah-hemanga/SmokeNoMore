ade75c4b8d6adb9b1009da5d26410f14eb6d6e4a
hborah <hemangakrishnaborah@gmail.com>
2016-09-11 15:23:38 -0700

    Adding base files

---
+++ app/src/main/java/productions/funky/com/smokenomore/fragments/Graphs.java
@@ -0,0 +1,45 @@
+package productions.funky.com.smokenomore.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import productions.funky.com.smokenomore.R;
+
+/**
+ * Created by hborah on 9/11/16.
+ */
+public class Graphs extends Fragment {
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static Graphs newInstance(int sectionNumber) {
+        Graphs fragment = new Graphs();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public Graphs() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_counter, container, false);
+        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+        textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+        return rootView;
+    }
+}
