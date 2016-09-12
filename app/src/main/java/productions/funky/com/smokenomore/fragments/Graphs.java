7d832c5787ad9099a0734d0f0aca8f028f938151
hborah <hemangakrishnaborah@gmail.com>
2016-09-11 19:09:40 -0700

    Added boundaries

--- app/src/main/java/productions/funky/com/smokenomore/fragments/Graphs.java
+++ app/src/main/java/productions/funky/com/smokenomore/fragments/Graphs.java
@@ -1,11 +1,19 @@
 package productions.funky.com.smokenomore.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.ValueDependentColor;
+import com.jjoe64.graphview.helper.StaticLabelsFormatter;
+import com.jjoe64.graphview.series.BarGraphSeries;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -19,6 +27,8 @@ import productions.funky.com.smokenomore.database.Counter;
  * Created by hborah on 9/11/16.
  */
 public class Graphs extends Fragment {
+    private View rootView;
+
     /**
      * The fragment argument representing the section number for this
      * fragment.
@@ -57,13 +67,72 @@ public class Graphs extends Fragment {
         }
     }
 
+    private void drawLast7Months() {
+        //GraphView graph = (GraphView) rootView.findViewById(R.id.graph_5_months);
+        GraphView line_graph = (GraphView) rootView.findViewById(R.id.graph_5_months);
+        BarGraphSeries <DataPoint> line_series = new BarGraphSeries <DataPoint >
+                        (new DataPoint[] {
+                new DataPoint(0, 17),
+                new DataPoint(1, 5),
+                new DataPoint(2, 45),
+                new DataPoint(3, 5),
+                new DataPoint(4, 3)
+        });
+
+        line_graph.addSeries(line_series);
+        StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(line_graph);
+        staticLabelsFormatter.setHorizontalLabels(new String[] {"Jan", "Feb", "March"});
+        staticLabelsFormatter.setVerticalLabels(new String[]{"Sun", "Mon", "Tue"});
+        line_graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
+    }
+
+    private void drawLastSevenDays() {
+        GraphView graph = (GraphView) rootView.findViewById(R.id.graph_7_days);
+        BarGraphSeries<DataPoint> series = new BarGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(-6, 1),
+                new DataPoint(-5, 5),
+                new DataPoint(-4, 3),
+                new DataPoint(-3, 2),
+                new DataPoint(-2, 6),
+                new DataPoint(-1, 2),
+                new DataPoint(0, 1),
+                //new DataPoint(5, 5),
+                //new DataPoint(6, 6),
+        });
+
+        graph.addSeries(series);
+        graph.getViewport().setMaxX(1);
+        graph.getGridLabelRenderer().setGridColor(0xFFFFFF);
+        graph.getGridLabelRenderer().setVerticalLabelsColor(0xFFFFFF);
+        graph.getGridLabelRenderer().setHorizontalLabelsColor(0xFFFFFF);
+
+        graph.getViewport().setXAxisBoundsManual(true);
+
+        // styling
+        series.setValueDependentColor(new ValueDependentColor<DataPoint>() {
+            @Override
+            public int get(DataPoint data) {
+                return Color.rgb((int) data.getX() * 255 / 4, (int) Math.abs(data.getY() * 255 / 6), 100);
+            }
+        });
+
+        series.setSpacing(20);
+
+        series.setDrawValuesOnTop(true);
+        series.setValuesOnTopColor(Color.RED);
+    }
+
+    private void drawGraphs() {
+        drawLastSevenDays();
+        drawLast7Months();
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.graphs, container, false);
-        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-        textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+        rootView = inflater.inflate(R.layout.graphs, container, false);
         testDB();
+        drawGraphs();
         return rootView;
     }
 }
